ba9f51ba54021ad9d72b44cb40db660c57493caf
AntonPlish <antonplish@gmail.com>
2022-09-05 14:10:59 +0300

    Commit hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,57 +1,65 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import Section from './Section/Section'
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Statistics from './Statistics/Statistics';
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0
-  }
-  
-  onClickFeedback = name => {
-    this.setState(prevState => {
-      return { [name]: prevState[name] + 1 };
-    });
+
+export default function App() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const onClickFeedback = (name) => {
+    switch (name) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+      default:
+        return;
+    }
   };
   
-  options = ['good', 'neutral', 'bad'];
+  const options = ['good', 'neutral', 'bad'];
   
-  totalAmount = () => {
-    return this.state.good + this.state.neutral + this.state.bad
+  const totalAmount = () => {
+    return good + neutral + bad
   }
   
-  totalPercentageAmount = () => {
-    return Math.round(this.state.good * 100 / this.totalAmount() || 0)
+  const totalPercentageAmount = () => {
+    return Math.round(good * 100 / totalAmount() || 0)
   };
-  
-  render() {
-    return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'block',
-          justifyContent: 'center',
-          fontSize: 40,
-          color: '#010101'
-        }}
-      >
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            buttons={this.options}
-            onClickFeedback={this.onClickFeedback}
-          />
-        </Section>
-        <Section title="Statistics">
-          <Statistics
-            good={this.state.good}
-            neutral={this.state.neutral}
-            bad={this.state.bad}
-            total={this.totalAmount()}
-            percentage={this.totalPercentageAmount()}
-          />
-        </Section>
-      </div>
-    );
-  }
+
+
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'block',
+        justifyContent: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          buttons={options}
+          onClickFeedback={onClickFeedback}
+        />
+      </Section>
+      <Section title="Statistics">
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          total={totalAmount()}
+          percentage={totalPercentageAmount()}
+        />
+      </Section>
+    </div>
+  );
 }
